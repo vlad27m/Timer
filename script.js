a84ea40b724d277601ffb3ec32ffc3a5884459af
elenays <helenasp@yandex.ru>
2021-03-31 23:15:09 +0300

    add setInterval

--- script.js
+++ script.js
@@ -1,25 +1,34 @@
-const time = document.querySelector('[id="time"]');
-const element = document.querySelector('[id="element"]');
-const btn = document.querySelector('[id="btn"]');
+const time = document.querySelector('[id="time"]')
+const element = document.querySelector('[id="element"]')
+const btn = document.querySelector('[id="btn"]')
 
 
 function start() {
-    setTimeout(trigger, time.value*1000);
-  time.disabled = true;
- btn.disabled = true
+	setTimeout(trigger, time.value * 1000)
+	time.disabled = true
+	btn.disabled = true
+
+	const showTime = setInterval(() => {
+		time.value -= 1
+		if (time.value <= 0) {
+			clearInterval(showTime)
+			return
+		}
+		console.log('time', time.value)
+	}, 1000)
 }
 
 function trigger() {
-document.body.style.backgroundColor = "#AA0000";
-  element.classList.toggle('hidden')
-} 
+	document.body.style.backgroundColor = "#AA0000"
+	element.classList.toggle('hidden')
+}
 
-function hide() {  
-  document.body.style.backgroundColor = "#ffffff";
-  element.classList.toggle('hidden');
-  time.value = "0";
-  time.disabled = false;
-  btn.disabled = false
-} 
+function hide() {
+	document.body.style.backgroundColor = "#ffffff"
+	element.classList.toggle('hidden')
+	time.value = "0"
+	time.disabled = false
+	btn.disabled = false
+}
 
 hide()
